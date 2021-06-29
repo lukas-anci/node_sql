4d0988f6b6b6d7c2aeb688bf80487b1e07e0a6f9
Lukas <lukas.ancikevicius@gmail.com>
2021-06-29 11:20:18 +0300

    create post

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-// start
+// this is it
 const express = require('express');
 const morgan = require('morgan');
 const mysql = require('mysql');
+
 // set up connection
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
+
   database: 'nodesql',
 });
 
 // connect
 db.connect((err) => {
   if (err) throw err;
-  console.log('Connected to database!');
+  console.log('Connected to database !!!');
 });
 
 const app = express();
-// middleware
+
+// middleWare
 app.use(morgan('common'));
 
 app.get('/', (req, res) => {
-  res.send('Express veikia');
+  res.send('Express veikia normaliai');
 });
 
 // create database
@@ -32,6 +35,37 @@ app.get('/createdb', (req, res) => {
     // no errors
     console.log(result);
     res.send('nodesql duomenu baze sukurta');
+  });
+});
+
+// create table
+app.get('/table/create', (req, res) => {
+  const sql = `
+  CREATE TABLE posts(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    created_at TIMESTAMP on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+)
+  `;
+  db.query(sql, (err, result) => {
+    if (err) {
+      res.send(err.stack);
+      throw err;
+    }
+    console.log(result);
+    res.json({ msg: 'lentele sukurta', result });
+  });
+});
+
+// add new post
+app.get('/newpost', (req, res) => {
+  const newPost = { title: 'Third post title', body: 'Third post body' };
+  const sql = 'INSERT INTO posts SET ?';
+  db.query(sql, newPost, (err, result) => {
+    if (err) throw err.stack;
+    res.json({ msg: 'irasas sukurtas', result });
   });
 });
 
